886a6c8b1ff41320a2cae65c244565e76bc55579
sanjay <ssah6694@gmail.com>
2022-03-09 22:15:56 +0530

    sanjay added some data

--- Employ/Employ/Program.cs
+++ Employ/Employ/Program.cs
@@ -2,11 +2,18 @@
 
 namespace Employ
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Program.MyMethod();
+        }
+        static void MyMethod()
+        {
+            Console.WriteLine("this is my good practice");
         }
     }
+    
+
 }
