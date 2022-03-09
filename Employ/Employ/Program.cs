e03da3eb6d3af4659ab08d2e7a04c1610ca4e078
sanjay <ssah6694@gmail.com>
2022-03-09 21:48:35 +0530

    i have added main method only

---
+++ Employ/Employ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
